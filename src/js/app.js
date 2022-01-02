9b2702a149a8fcd21d01c2399f369a799f81fa81
Elena Bravo <hey@elenabravo.com>
2022-01-02 03:18:00 +0100

    fix import

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,8 @@
 import '../css/global.css'
 import { onDocumentReady, importAll } from './functions.js'
 
-// import all media from ./src/assets
-importAll(
-  require.context(
-    '../assets/images',
-    false,
-    /\.(png|jpe?g|svg|gif|mov|mp4|ico|xml|webmanifest)$/
-  )
-)
+// import all images from ./src/assets
+importAll(require.context('../assets/images', false, /\.(png|jpe?g|svg|gif)$/))
 
 onDocumentReady(function () {
   console.log('hello friend.')
