bbbcbda98a9ccf1e7c27c0fc9fa304e69a7b1490
Elena Bravo <36118057+elenabravo@users.noreply.github.com>
2022-01-02 12:10:41 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import '../css/global.css'
 import { onDocumentReady, importAll } from './functions.js'
 
-// import all images from ./src/assets
+// import all images from assets
 importAll(require.context('../assets/images', false, /\.(png|jpe?g|svg|gif)$/))
 
 onDocumentReady(function () {
